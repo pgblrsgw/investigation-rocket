30f5d537d712950697d59b2318791c89672db895
Geordon Worley <vadixidav@gmail.com>
2017-02-09 10:43:13 -0500

    add dependency with revision/git for a4-proto; also add serde_json and rocket to dependencies and create simple web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate serde_json;
+extern crate a4_proto as proto;
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }

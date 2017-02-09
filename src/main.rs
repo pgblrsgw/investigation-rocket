05c7c36f30c96f847916fec5da0706b2db873712
Geordon Worley <vadixidav@gmail.com>
2017-02-09 12:42:12 -0500

    implement parts of API and add one test

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,71 @@
 
 extern crate serde_json;
 extern crate a4_proto as proto;
-
+#[macro_use]
+extern crate lazy_static;
 extern crate rocket;
+extern crate rocket_contrib;
+
+use std::collections::hash_map::{HashMap, Entry};
+use std::sync::Mutex;
+use rocket_contrib::JSON;
+use rocket::http::Status;
+
+#[derive(Default)]
+struct Problem {
+    obstacles: Vec<proto::Obstacle>,
+}
+
+lazy_static! {
+    static ref MAP: Mutex<HashMap<String, Problem>> = Mutex::new(HashMap::new());
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn query_problems() -> JSON<Vec<String>> {
+    JSON(MAP.lock().unwrap().keys().cloned().collect())
+}
+
+#[post("/<name>")]
+fn create_problem(name: &str) -> Result<(), Status> {
+    // Attempt to create the new problem.
+    match MAP.lock().unwrap().entry(String::from(name)) {
+        // The entry was already taken, so return conflict status code.
+        Entry::Occupied(_) => Err(Status::Conflict),
+        // The entry was vacant, so insert a blank problem.
+        Entry::Vacant(v) => {
+            v.insert(Problem::default());
+            Ok(())
+        }
+    }
+}
+
+fn new_mounted_rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![query_problems, create_problem])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    new_mounted_rocket().launch();
+}
+
+#[cfg(test)]
+mod test {
+    use super::serde_json;
+    use rocket::testing::MockRequest;
+    use rocket::http::{Status, Method};
+
+    #[test]
+    fn problems() {
+        let rocket = super::new_mounted_rocket();
+        let mut request = MockRequest::new(Method::Get, "/");
+        let mut response = request.dispatch_with(&rocket);
+        // This should always be status 200.
+        assert_eq!(response.status(), Status::Ok);
+
+        // Write the body out as a string.
+        let obstacles: Option<Vec<String>> = response.body()
+            .and_then(|b| b.into_string())
+            .and_then(|s| serde_json::from_str(&s).expect("Failed to parse body as JSON"));
+
+        assert_eq!(obstacles, Some(vec![]));
+    }
 }
